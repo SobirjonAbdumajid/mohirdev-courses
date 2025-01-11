fd89bb9aa9d53c4147c95b6aa3bb033a7dce44ea
Sobirjon005 <sobirjon0305@gmail.com>
2025-01-11 15:01:58 +0500

    t

--- 02-mohirdev-python/12-9yanvar/task1.py
+++ 02-mohirdev-python/12-9yanvar/task1.py
@@ -1,9 +1,45 @@
-my_input = ['d', 't', 'a','g', 'q', 'i', 'i']
-variable = 'a'
+# my_input = ['d', 't', 'a','g', 'q', 'i', 'i']
+# variable = 'a'
 
-for i, value in enumerate(my_input):
-    if value == variable:
-        my_input.remove(my_input[i])
-    print(my_input[i])
+# for i, value in enumerate(my_input):
+#     if value == variable:
+#         my_input.remove(my_input[i])
+#     print(my_input[i])
     
-print(my_input)
+# print(my_input)
+
+
+# --------------------
+
+my_list = ['d', 't', 'ag', 'g', 4,'d', 'a']
+big_list = []
+
+for i in range(0, len(my_list)-1, 2):
+    big_list.append([my_list[i], my_list[i+1]])
+             
+if len(my_list)%2==1:
+    big_list.append([my_list[-1]])
+
+print(big_list)
+
+
+
+
+# my_list = ['d', 't', 'ag', 'g']
+# big_list = []
+
+# # Iterate through the list in steps of 2
+# for i in range(0, len(my_list), 2):
+#     # If the next element exists, pair the current and next element
+#     if i + 1 < len(my_list):
+#         big_list.append([my_list[i], my_list[i + 1]])
+#     else:
+#         big_list.append([my_list[i]])
+
+# print(big_list)
+
+
+# first_word = "afds, new_order.address".split(',')[0].strip()
+# print
+
+    # get_order_price(first_word)
